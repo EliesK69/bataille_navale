56ee7a48b0ccd349e55389a2448f708760d2b6bd
3ami Chifour <3ami@chifour.com>
2023-05-23 10:13:22 +0200

    ajout html/css

--- js/script.js
+++ js/script.js
@@ -1,6 +1,59 @@
+//attraper les éléments
+const
+
 // Déclaration des variables
-const monNavire = {
-    modele: ["corvette","sous-marin","frégate","porte-avion"],
-    longueur: [2,3,4,4],
-    largeur: [1,1,1,2],
-};
+const tabJoueur = [
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0]
+];
+const tabOrdi = [
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0]
+];
+
+const elementsDeplacables = document.querySelectorAll(".drag");
+const ouDropper = document.querySelectorAll(".ouDropper");
+
+let elementDeplace = null;
+let positionBateauxJoueurs = [];
+
+elementsDeplacables.forEach(function(element) {
+  element.addEventListener('dragstart', function(event) {
+    elementDeplace = event.target;
+    event.dataTransfer.setData('youpi', event.target.id);
+  });
+});
+
+ouDropper.forEach(function(dropZone) {
+  dropZone.addEventListener('dragover', function(event) {
+    event.preventDefault();
+  });
+
+  dropZone.addEventListener('drop', function(event) {
+    if (elementDeplace !== null) {
+      let elementDestination = event.target;
+      
+      positionBateauxJoueurs.push(elementDestination.id) 
+      console.log(positionBateauxJoueurs) 
+
+      while (!elementDestination.classList.contains("ouDropper")) {
+        elementDestination = elementDestination.parentNode;
+      }
+      elementDestination.appendChild(elementDeplace);
+      elementDeplace = null;
+    }
+  });
+});
+
+function setJoueur(){
+    if
+}
+
+function setOrdi(){
+    let ordiLocation = tabOrdi[Math.floor(tabOrdi.length * Math.random)][Math.floor(tabOrdi.length * Math.random)];
+}
+Math.random
