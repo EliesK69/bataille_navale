be05fc5afe874f655dfdbf673b5bc5b37a5719b1
3ami Chifour <3ami@chifour.com>
2023-07-03 11:18:08 +0200

    rajout images

--- js/script.js
+++ js/script.js
@@ -1,4 +1,37 @@
 //attraper les éléments
+const elementsDeplacables = document.querySelectorAll(".drag");
+const ouDropper = document.querySelectorAll(".ouDropper");
+
+let elementDeplace = null;
+let positionBateauxJoueurs = [];
+
+elementsDeplacables.forEach(function(element) {
+  element.addEventListener('dragstart', function(event) {
+    elementDeplace = event.target;
+    event.dataTransfer.setData('youpi', event.target.id);
+  });
+});
+
+ouDropper.forEach(function(dropZone) {
+  dropZone.addEventListener('dragover', function(event) {
+    event.preventDefault();
+  });
+
+  dropZone.addEventListener('drop', function(event) {
+    if (elementDeplace !== null) {
+      let elementDestination = event.target;
+      
+      positionBateauxJoueurs.push(elementDestination.id) 
+      console.log(positionBateauxJoueurs) 
+
+      while (!elementDestination.classList.contains("ouDropper")) {
+        elementDestination = elementDestination.parentNode;
+      }
+      elementDestination.appendChild(elementDeplace);
+      elementDeplace = null;
+    }
+  });
+});
 
 
 // Déclaration des variables
